5c1a1335e28972fec699ac458f01d7c84e7d332e
NovikovKonstantin-git <kostya4py@gmail.com>
2023-04-06 13:41:40 +0300

    fix api

--- illumail/api/urls.py
+++ illumail/api/urls.py
@@ -3,5 +3,8 @@ from .views import *
 
 
 urlpatterns = [
-    path('api/courses/', CoursesApiView.as_view()),
+    path('api/courses/', CoursesAPIList.as_view()),
+    path('api/courses/<int:pk>/update/', CoursesAPIUpdate.as_view()),
+    path('api/courses/<int:pk>/delete/', CoursesAPIDestroy.as_view()),
 ]
+
--- illumail/api/views.py
+++ illumail/api/views.py
@@ -3,6 +3,16 @@ from .serializers import CoursesSerializer
 from courses.models import Courses
 
 
-class CoursesApiView(generics.ListAPIView):
+class CoursesAPIList(generics.ListCreateAPIView):
+    queryset = Courses.objects.all()
+    serializer_class = CoursesSerializer
+
+
+class CoursesAPIUpdate(generics.RetrieveUpdateAPIView):
+    queryset = Courses.objects.all()
+    serializer_class = CoursesSerializer
+
+
+class CoursesAPIDestroy(generics.RetrieveDestroyAPIView):
     queryset = Courses.objects.all()
     serializer_class = CoursesSerializer
